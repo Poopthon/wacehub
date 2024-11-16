30c312f30da328597b29104dc2a255300e12b48e
Griffin Castles <rbxmidnightific@gmail.com>
2024-11-16 16:08:57 +0800

    Remove pnpm + fix splash page

--- pages/_closed.tsx
+++ pages/_closed.tsx
@@ -1,6 +1,6 @@
 export default function Closed() {
   return (
-    <div className="flex flex-col items-center min-h-[calc(100vh-44px)] justify-center bg-gray-100">
+    <div className="flex flex-col items-center h-screen justify-center bg-gray-100">
       <img src="/scasalogo.png" alt="SCSA Logo" className="w-32 h-32" />
       <h1 className="text-5xl tracking-tight max-w-3xl font-semibold mb-4 mt-10">
         We&apos;ll be back.
