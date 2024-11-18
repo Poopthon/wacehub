a9ddc5b6c74b5310704bbb893f1897d975eb7c07
Griffin Castles <rbxmidnightific@gmail.com>
2024-11-18 22:38:14 +0800

    Cleanup somewhat + branding

--- pages/_closed.tsx
+++ pages/_closed.tsx
@@ -1,7 +1,11 @@
 export default function Closed() {
   return (
     <div className="flex flex-col items-center h-screen justify-center bg-gray-100">
-      <img src="/wacehublogo.png" alt="SCSA Logo" className="w-32 h-32" />
+      <img
+        src="/wacehubbannerwebsite.png"
+        alt="SCSA Logo"
+        className="w-30 h-30 mb-[-7rem]"
+      />
       <h1 className="text-5xl tracking-tight max-w-3xl font-semibold mb-4 mt-10">
         We&apos;ll be back.
       </h1>
