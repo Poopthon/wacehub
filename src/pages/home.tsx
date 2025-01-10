d3103eca5fa761f437880e35db679bf11dfcd00c
Griffin Castles <rbxmidnightific@gmail.com>
2025-01-10 16:52:13 +0800

    Hopefully fix items?

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -43,16 +43,16 @@ export default function Home() {
 
         <div className="flex sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium">
           <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-4 text-gray-700 dark:text-white hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
             href="https://github.com/Poopthon/wacehub"
             target="_blank"
             rel="noopener noreferrer"
           >
-            GitHub <FaGithub />
+            GitHub <FaGithub className="text-gray-700 dark:text-white" />
           </a>
 
           <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-4 text-gray-700 dark:text-white hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
             href="https://www.youtube.com/channel/UCwt78lxMRd5q1rLpKeZwCTQ"
             target="_blank"
             rel="noopener noreferrer"
